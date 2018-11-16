cb562924bc53cb70678c31023d7c924186c59ace
PaulineMauryL <paulinemaurylaribiere@gmail.com>
2018-11-16 12:11:35 +0100

    merge

--- our_functions.py
+++ our_functions.py
@@ -99,3 +99,37 @@ def sentence_to_avg(tweet, word_to_vec_map):
     avg = avg/len(words)
     
     return avg
+
+def predict(X, Y, W, b, word_to_vec_map):
+    """
+    Given X (sentences) and Y (emoji indices), predict emojis and compute the accuracy of your model over the given set.
+    
+    Arguments:
+    X -- input data containing sentences, numpy array of shape (m, None)
+    Y -- labels, containing index of the label emoji, numpy array of shape (m, 1)
+    
+    Returns:
+    pred -- numpy array of shape (m, 1) with your predictions
+    """
+    m = X.shape[0]
+    pred = np.zeros((m, 1))
+    
+    for j in range(m):                       # Loop over training examples
+        
+        # Split jth test example (sentence) into list of lower case words
+        words = X[j].lower().split()
+        
+        # Average words' vectors
+        avg = np.zeros((20,))
+        for w in words:
+            avg += word_to_vec_map[w]
+        avg = avg/len(words)
+
+        # Forward propagation
+        Z = np.dot(W, avg) + b
+        A = softmax(Z)
+        pred[j] = np.argmax(A)
+        
+    print("Accuracy: "  + str(np.mean((pred[:] == Y.reshape(Y.shape[0],1)[:]))))
+    
+    return pred
