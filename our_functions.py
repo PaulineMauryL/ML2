9174b727ea712926606a76d3a03f240f4306c270
PaulineMauryL <paulinemaurylaribiere@gmail.com>
2018-11-18 10:36:57 +0100

    simplified file

--- our_functions.py
+++ our_functions.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import pickle
 from sklearn.utils import shuffle
 
 def read_data(data):
@@ -90,6 +91,67 @@ def sentence_to_avg(tweet, word_to_vec_map,size=20):
         avg = avg/nb
     
     return avg
+
+
+
+def model(X, Y, word_to_vec_map, learning_rate = 0.01, num_iterations = 400):
+    """
+    Model to train word vector representations in numpy.
+    
+    Arguments:
+    X -- input data, numpy array of sentences as strings, of shape (m, 1)
+    Y -- labels, numpy array of integers between -1 and 1, numpy-array of shape (m, 1)
+    word_to_vec_map -- dictionary mapping every word in a vocabulary into its 20-dimensional vector representation
+    learning_rate -- learning_rate for the stochastic gradient descent algorithm
+    num_iterations -- number of iterations
+    
+    Returns:
+    pred -- vector of predictions, numpy-array of shape (m, 1)
+    W -- weight matrix of the softmax layer, of shape (n_y, n_h)
+    b -- bias of the softmax layer, of shape (n_y,)
+    """
+    
+    np.random.seed(1)
+
+    # Define number of training examples
+    m = len(Y)                              # number of training examples
+    n_y = 2                                 # number of classes  
+    n_h = 20                                # dimensions of the embeddings vectors 
+    
+    # Initialize parameters using Xavier initialization
+    W = np.random.randn(n_y, n_h) / np.sqrt(n_h)
+    b = np.zeros((n_y,))
+    
+    # Convert Y to Y_onehot with n_y classes
+    Y_oh = one_hot(Y) 
+    
+    # Optimization loop
+    for t in range(num_iterations):                       # Loop over the number of iterations
+        for i in range(m):                                # Loop over the training examples
+        
+            # Average the word vectors of the words from the i'th training example
+            avg = sentence_to_avg(X[i], word_to_vec_map)
+
+            # Forward propagate the avg through the softmax layer
+            z = W @ avg + b
+            a = softmax(z)
+
+            # Compute cost using the i'th training label's one hot representation and "A" (the output of the softmax)
+            cost = - np.sum(Y_oh[i]*np.log(a))
+            
+            # Compute gradients 
+            dz = a - Y_oh[i]
+            dW = np.dot(dz.reshape(n_y,1), avg.reshape(1, n_h))
+            db = dz
+
+            # Update parameters with Stochastic Gradient Descent
+            W = W - learning_rate * dW
+            b = b - learning_rate * db
+        
+        if t % 10 == 0:
+            print("Epoch: " + str(t) + " --- cost = " + str(cost))
+            pred = predict(X, Y, W, b, word_to_vec_map)
+    return W, b
 
 
 def predict(X, Y, W, b, word_to_vec_map):
