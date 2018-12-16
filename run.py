eff3b546409a73fd413c864eb8b4d51cc4a34990
JeromeSavary <43443094+JeromeSavary@users.noreply.github.com>
2018-12-16 22:45:46 +0100

    run.py in progress, need to debug load_model

--- run.py
+++ run.py
@@ -1,22 +1,23 @@
 import numpy as np
 import pandas as pd
 
-import run_fct
-import LSTM_functions
+from run_fct import *
+from LSTM_functions import *
 
 import tensorflow.keras as keras
 from keras.models import load_model
 
-def generate_submission_from_model():
+def generate_submission_from_model(X_train, X_test, ids):
     # Load the 200-d twitter dictionnary 
     word_to_index, index_to_word, word_to_vec_map = read_glove_vecs_only_alpha('dictionnary/glove.twitter.27B.200d.txt')
 
     # Maximum length of the sentence (computed on the training set)
-    max_length = 11 #???!!! On laisse 11 ou on met la fonction get_max_length ?
+    #max_length = 11 #???!!! On laisse 11 ou on met la fonction get_max_length ?
+    max_length = get_max_length(X_train)
 
-    # Load the testing set
-    ids = get_ids("twitter-datasets/test.txt")
-    X_test = read_data("twitter-datasets/test.txt")
+    # Load the testing set #EDIT : I don't think we need that, easier in input
+    #ids = get_ids("twitter-datasets/test.txt")
+    #X_test = read_data("twitter-datasets/test.txt")
 
     # Pre-process the testing set
     ############
